05908cd8b5dec9011291f8faa812877cec71bc6b
WalkOrRun <rettuu1@hotmail.com>
2020-04-14 17:41:34 -0500

    2 files modified

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Account} from '../Account';
+
 import {HttpClientModule} from '@angular/common/http';
 import {AccountService} from '../account.service';
+import { Account} from '../Account';
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl} from '@angular/forms';
+
+import {HttpClientModule} from '@angular/common/http';
+import {AccountService} from '../account.service';
+import { Account} from '../Account';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
