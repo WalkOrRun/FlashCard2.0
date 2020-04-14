8a8b39b34f8deb11ed805408b9edd1818b70eb64
WalkOrRun <rettuu1@hotmail.com>
2020-04-14 14:20:12 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subject } from "rxjs/Subject";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/Observable";
 export type QuizPage = 'create' | 'home' | 'list';
 @Component({
   selector: 'my-app',
@@ -19,4 +22,33 @@ export class AppComponent  {
   toggleEditor(type: QuizPage) {
     this.quiz = type;
   }
+    private isMobile = new Subject();
+    public screenWidth: string;
+
+
+    constructor() {
+        this.checkWidth();
+    }
+
+    onMobileChange(status: boolean) {
+        this.isMobile.next(status);
+    }
+
+    getMobileStatus(): Observable<any> {
+        return this.isMobile.asObservable();
+    }
+
+    public checkWidth() {
+        var width = window.innerWidth;
+        if (width <= 768) {
+            this.screenWidth = 'sm';
+            this.onMobileChange(true);
+        } else if (width > 768 && width <= 992) {
+            this.screenWidth = 'md';
+            this.onMobileChange(false);
+        } else {
+            this.screenWidth = 'lg';
+            this.onMobileChange(false);
+        }
+    }  
 }
